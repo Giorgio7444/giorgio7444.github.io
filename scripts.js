96c565752c9e0e9331958a91b8c1bf1d62f2c5e5
Giorgio Pulli <giorgiopulli@gmail.com>
2025-03-24 23:21:40 +0100

    Tolto Console.Log e Aggiunto LinkedIn

--- scripts.js
+++ scripts.js
@@ -109,5 +109,3 @@ var swiper = new Swiper(".mySwiper", {
 })
 
 });
-
-console.log("Progetto d'esame di Pulli Giorgio per il corso di Linguaggi Multimediali 1 tenuto dal prof Molinari Antonio - Digital Design and Communication, LABA Brescia - A.A. 2024/2025");
